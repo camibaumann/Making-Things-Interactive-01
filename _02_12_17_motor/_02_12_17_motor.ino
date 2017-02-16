267f1202487aea3746e60ad4b89f61e13b5f552b
camibaumann <cbaumann@andrew.cmu.edu>
2017-02-16 00:57:31 -0500

    change -3

---
+++ _02_12_17_motor/_02_12_17_motor.ino
@@ -0,0 +1,48 @@
+/* based on code from Arduino projects book, Project 10 "Zoetrope", p107
+ *  changed by Camille Baumann-Jaeger
+*/
+
+const int controlPin1 = 2;
+const int enablePin = 9;
+const int onOffSwitchStateSwitchPin = 5;
+const int potPin= A0;
+int onOffSwitchState=0;
+int previousOnOffSwitchState=0;
+int motorEnabled=0;
+int motorSpeed=0;
+int motorDirection=1;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(onOffSwitchStateSwitchPin, INPUT);
+  pinMode(controlPin1, OUTPUT);
+  pinMode(enablePin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  onOffSwitchState = digitalRead(onOffSwitchStateSwitchPin);
+  delay(1);
+  motorSpeed = analogRead(potPin)/4;
+  if (onOffSwitchState != previousOnOffSwitchState) {
+    if (onOffSwitchState == HIGH) {
+      motorEnabled = !motorEnabled;
+    }
+  }
+  if (motorDirection == 1) {
+    digitalWrite(controlPin1, HIGH);
+    digitalWrite(controlPin2, LOW);   
+    }
+  else {
+    digitalWrite(controlPin1, LOW);
+    digitalWrite(controlPin2, HIGH);
+  }
+  if (motorEnabled == 1) {
+    analogWrite(enablePin, motorSpeed);
+  }
+  else {
+    analogWrite(enablePin, 0);
+  }
+  previousOnOffSwitchState = onOffSwitchState;
+}
